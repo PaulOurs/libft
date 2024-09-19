aff6398709cbb18602d5d173d93ae32d1a60d3ab
Paul Bello <pb74coc@gmail.com>
2024-09-19 22:15:21 +0200

    ft_toupper, ft_tolower, ft_strchr, ft_strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paubello <paubello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 21:54:37 by paubello          #+#    #+#             */
+/*   Updated: 2024/09/19 21:54:37 by paubello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s) - 1;
+	if ((char)c == '\0')
+		return ((char *)s + i + 1);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
+}
