25d829adb7dc5e09c445a5b8e73ba96e74de7cae
paubello <paubello@student.42lyon.fr>
2024-11-07 13:50:24 +0100

    Changed char * to unsigned char *

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: paubello <paubello@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 06:07:20 by paubello          #+#    #+#             */
-/*   Updated: 2024/11/05 01:07:45 by paubello         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:49:57 by paubello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	char	*s;
-	char	*d;
+	size_t			i;
+	unsigned char	*s;
+	unsigned char	*d;
 
 	i = 0;
-	s = (char *)src;
-	d = (char *)dest;
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
 	while (i < n)
 	{
 		d[i] = s[i];
