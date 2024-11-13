f5ce7c80848e75a379ba12d71c87bd75e3a3a320
PaulOurs <pb74coc@gmail.com>
2024-11-13 02:09:44 +0100

    Made atoi in long

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: paubello <paubello@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 02:16:38 by paubello          #+#    #+#             */
-/*   Updated: 2024/11/05 01:05:40 by paubello         ###   ########.fr       */
+/*   Updated: 2024/11/13 02:09:15 by paubello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	result;
+	int		sign;
+	long	i;
+	long	result;
 
-	i = 0;
 	sign = 1;
+	i = 0;
 	result = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
@@ -34,5 +34,5 @@ int	ft_atoi(const char *str)
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	return (result * sign);
+	return ((int)(result * sign));
 }
